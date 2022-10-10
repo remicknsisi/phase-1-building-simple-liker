8164278e1c7d0ee6d1eef560596b7cbd17e73e9e
Sienna <remicknsisi@gmail.com>
2022-10-10 18:03:29 -0400

    commit tree f58251061e5fc153632ef197bd9df35aafc44280

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.querySelector('#modal').hidden
 
 
 
+// You will invoke mimicServerCall() in response to a user action, and the function will randomly return either a "success" or "fail" response. Your code will then need to handle the response appropriately: updating the appearance of the heart if it returns a "successful" response, and displaying an error in the DOM otherwise.
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
